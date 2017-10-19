ed0f44896837db2d1176377045f92fb7294afaf0
Anna Zander <annadzander@gmail.com>
2017-10-19 09:48:20 -0500

    added to code for calculating GC

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -2,9 +2,10 @@
 houseelf.data <- read.csv('./Data/houseelf_earlength_dna_data_1.csv')
 
 #calculates GC content regardless of case
+library(stringr)
 
 gc_content <- function(sequence){
-  str_to_lower(sequence)
+  sequence <- str_to_lower(sequence)
   gs <- str_count(sequence, "g")
   cs <- str_count(sequence, "c")
   gc_content <- (Gs + Cs) / str_length(sequence) * 100 
