cec859b47e57b03f599b3f40c55385b583e3bd89
Anna Zander <annadzander@gmail.com>
2017-10-19 09:44:50 -0500

    added code to calculate GC content of DNA regardless of case

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -1,2 +1,12 @@
 ##Analysis intended to describe the relationship between GC content and length of ears.
 houseelf.data <- read.csv('./Data/houseelf_earlength_dna_data_1.csv')
+
+#calculates GC content regardless of case
+
+gc_content <- function(sequence){
+  str_to_lower(sequence)
+  gs <- str_count(sequence, "g")
+  cs <- str_count(sequence, "c")
+  gc_content <- (Gs + Cs) / str_length(sequence) * 100 
+  return(gc_content)
+}
